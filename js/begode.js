81fb13cf1c1cc7688215ced2b34b46829d1a76e0
Bill Kolokithas <kolokithas.b@gmail.com>
2022-03-29 10:45:03 +0300

    Add EX 20s variants

--- js/begode.js
+++ js/begode.js
@@ -13,7 +13,8 @@ function modelParams() {
     case 'RS C30':      return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
     case 'RS C38':      return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
     case 'EX':          return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
-    case 'EX 20S':      return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
+    case 'EX20S C30':   return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
+    case 'EX20S C38':   return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
     case 'Monster':     return { 'voltMultiplier': 1.50, 'minCellVolt': 3.25 }
     case 'EXN':         return { 'voltMultiplier': 1.50, 'minCellVolt': 3.15 }
     case 'Monster Pro': return { 'voltMultiplier': 1.50, 'minCellVolt': 3.1 }
